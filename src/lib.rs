bf51b11040d01845ba25389da34fd4eb8bbf79ef
Gabriel Fioravante <61801407+gabrielfioravante@users.noreply.github.com>
2022-10-12 20:22:25 -0300

    refactor: remove logic from main

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,45 @@
-pub mod settings;
+use crate::{files::Files, http::HTTP, settings::Settings};
+use clap::Parser;
+use std::sync::Arc;
+
 pub mod files;
 pub mod http;
+pub mod settings;
+
+/// A simple program to send HTTP requests from .json files.
+#[derive(Parser, Debug)]
+#[command(name = "json-sender")]
+#[command(author = "Gabriel Fioravante")]
+#[command(version="0.1.0", about, long_about = None)]
+pub struct Args {
+    /// Config file path. Ex: "~/files/sender.toml".
+    #[arg(short, long)]
+    config: Option<String>,
+}
+
+pub async fn init() {
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Info)
+        .init();
+
+    log::info!("Starting JSON Sender");
+
+    let args = Args::parse();
+    let settings = Settings::new(args.config);
+
+    let files = Files::new(settings.target.clone(), settings.bindinds.clone());
+    let http = Arc::new(HTTP::new(settings));
+
+    let file_list = files.get_req_info_list();
+
+    log::info!("{} requests to send", file_list.len());
+
+    for f in file_list {
+        let h = Arc::clone(&http);
+        tokio::spawn(async move {
+            h.handle(f).await;
+        })
+        .await
+        .unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,4 @@
-use clap::Parser;
-use json_sender::files::Files;
-use json_sender::http::HTTP;
-use json_sender::settings::Settings;
-use std::sync::Arc;
-
-/// A simple program to send HTTP requests from .json files.
-#[derive(Parser, Debug)]
-#[command(name = "json-sender")]
-#[command(author = "Gabriel Fioravante")]
-#[command(version="0.1.0", about, long_about = None)]
-pub struct Args {
-    /// Config file path. Ex: "~/files/sender.toml".
-    #[arg(short, long)]
-    config: Option<String>,
-}
-
 #[tokio::main]
 async fn main() {
-    env_logger::init();
-
-    let args = Args::parse();
-
-    let settings = Settings::new(args.config);
-
-    let files = Files::new(settings.target.clone(), settings.bindinds.clone());
-    let http = Arc::new(HTTP::new(settings));
-
-    let file_list = files.get_req_info_list();
-
-    for f in file_list {
-        let h = Arc::clone(&http);
-        tokio::spawn(async move {
-            h.handle(f).await;
-        }).await.unwrap();
-    }
+    json_sender::init().await;
 }
